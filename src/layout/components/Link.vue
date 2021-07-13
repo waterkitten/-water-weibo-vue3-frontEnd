982ea40b33c88fc27b96b31e7819b3a1a22e8500
zhuliye <zhuliye@tencent.com>
2021-07-13 20:44:16 +0800

    feat:Link component

---
+++ src/layout/components/Link.vue
@@ -0,0 +1,28 @@
+<template>
+  <component :is="type" v-bind="linkProps(to)">
+    <slot />
+  </component>
+</template>
+<script>
+import { defineComponent } from 'vue';
+export default defineComponent({
+  name: 'Link',
+  props: {
+    to: {
+      type: String,
+      require: true,
+    },
+  },
+  setup(props) {
+    const linkProps = (to) => {
+      return {
+        to,
+      };
+    };
+    return {
+      type: 'router-link',
+      linkProps,
+    };
+  },
+});
+</script>
