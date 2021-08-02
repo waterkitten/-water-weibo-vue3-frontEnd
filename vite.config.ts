8727d0285fb821a56ce1b44fc85381f521a06aef
zhuliye <zhuliye@tencent.com>
2021-08-02 10:31:30 +0800

    feat:配置了本地开发时候的跨域设置

--- vite.config.ts
+++ vite.config.ts
@@ -35,7 +35,13 @@ const viteConfig: UserConfig = {
      */
     port: VITE_PORT,
     // 本地跨域代理
-    proxy: createProxy(VITE_PROXY)
+    proxy: {
+      '^/api': {
+        target: 'http://localhost:3002',
+        changeOrigin: true,
+        rewrite:path=>path.replace(/^\/api/,``)
+      }
+    }
   },
   plugins: [
     vue(),
